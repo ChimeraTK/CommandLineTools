488668e3ee3af5b3df6aef80a917cb1de92df78e
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2014-12-17 11:06:00 +0000

    code coverage is now set up for the project
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/CommandLineTools/trunk@697 5f601a5c-88b9-47f3-b851-818f6741d00d

--- tests/scripts/testNoParameters.sh
+++ tests/scripts/testNoParameters.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-./mtca4u 2> output.txt
+./mtca4u &> output.txt # Both stdout and stderr
+                       # are piped to file for now. Reason being the output of
+                       # this command right now prints to both streams. TODO:
+                       # Revisit this piping once code is revised.
 if [ $? -ne 1 ] ; then
     exit -1
 fi 

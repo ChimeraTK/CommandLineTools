cf686b11fd37415d67e06a504b9552c0c848404b
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-01-19 12:08:17 +0000

    adapted version number and fixed broken test
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/CommandLineTools/trunk@732 5f601a5c-88b9-47f3-b851-818f6741d00d

--- tests/scripts/testNoParameters.sh
+++ tests/scripts/testNoParameters.sh
@@ -8,7 +8,11 @@ if [ $? -ne 1 ] ; then
     exit -1
 fi 
 
+# we cannot pipe directly onto seed because otherwise the $? would give
+# the result of sed, not of mtca4u.
+cat output.txt | sed "{s/version [0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]/version IRRELEVANT/}" > output_without_version.txt
+
 #drop out if an error occurs
 set -e
 
-diff output.txt referenceNoParameters.txt
+diff output_without_version.txt referenceNoParameters.txt

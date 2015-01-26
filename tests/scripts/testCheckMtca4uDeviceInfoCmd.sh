c217ec80d1a714f807bb2811c4567488760aebf3
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-01-26 17:27:01 +0000

    modified testcase for missed  scenario
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/CommandLineTools/trunk@740 5f601a5c-88b9-47f3-b851-818f6741d00d

--- tests/scripts/testCheckMtca4uDeviceInfoCmd.sh
+++ tests/scripts/testCheckMtca4uDeviceInfoCmd.sh
@@ -7,14 +7,21 @@ mtca4u_executable=./mtca4u
 actual_console_output="./output_mtca4uDeviceInfo.txt"
 expected_console_output="./referenceTexts/referenceMtca4uDeviceInfo.txt"
 
-mtca4u_command="$mtca4u_executable device_info DUMMY1"
-
-$mtca4u_command > $actual_console_output  2>&1 # redirect
-                                               # both stderr and stdout to file 
-
-if [ $? -ne 0 ] ; then # 0 is the exit status for a successful command
+mtca4u_command_1="$mtca4u_executable device_info DUMMY1"
+$mtca4u_command_1 > $actual_console_output  2>&1 # redirect
+                                                 # both stderr and stdout to file 
+if [ $? -ne 0 ] ; then # 0 is the exit 
+                       # status for a successful command
     exit -1
 fi 
+
+mtca4u_command_2="$mtca4u_executable device_info"
+$mtca4u_command_2 >> $actual_console_output  2>&1 
+if [ $? -ne 1 ] ; then 
+    exit -1
+fi 
+
+
 
 diff $actual_console_output $expected_console_output
 

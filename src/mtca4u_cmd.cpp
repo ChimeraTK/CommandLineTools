a8850ec6dca8a4f301b4039855348e1ae832cb8b
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-05-20 10:36:54 +0000

    Code for Bug #1394: device_info does not print the module name
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/CommandLineTools/trunk@1015 5f601a5c-88b9-47f3-b851-818f6741d00d

--- src/mtca4u_cmd.cpp
+++ src/mtca4u_cmd.cpp
@@ -241,8 +241,14 @@ void getDeviceInfo(unsigned int argc, const char* argv[])
 
   unsigned int index = 0;
   for (std::vector<mapFile::mapElem>::const_iterator cit = map->begin(); cit != map->end(); ++cit, ++index) {
-    cout << cit->reg_name.c_str() << "\t" << cit->reg_elem_nr << "\t\t" << cit->reg_signed << "\t\t";
-    cout << cit->reg_width << "\t\t" << cit->reg_frac_bits << "\t\t\t" << " " << endl; // ToDo: Add Description
+      // print out module name if present
+      if(cit->reg_module.empty()){
+	  cout << cit->reg_name.c_str() << "\t";
+      } else {
+	  cout << cit->reg_module << "." << cit->reg_name.c_str() << "\t";
+      }
+      cout << cit->reg_elem_nr << "\t\t" << cit->reg_signed << "\t\t";
+      cout << cit->reg_width << "\t\t" << cit->reg_frac_bits << "\t\t\t" << " " << endl; // ToDo: Add Description
   }
 }
 
--- tests/scripts/testCheckMtca4uDeviceInfoCmd.sh
+++ tests/scripts/testCheckMtca4uDeviceInfoCmd.sh
@@ -6,16 +6,24 @@ mtca4u_executable=./mtca4u
 actual_console_output="./output_mtca4uDeviceInfo.txt"
 expected_console_output="./referenceTexts/referenceMtca4uDeviceInfo.txt"
 
-mtca4u_command_1="$mtca4u_executable device_info DUMMY1"
-$mtca4u_command_1 > $actual_console_output  2>&1 # redirect
+# print out device info for a card without modules
+echo "Device Info for card without modules" > $actual_console_output  2>&1
+$mtca4u_executable device_info DUMMY1 > $actual_console_output  2>&1 # redirect
                                                  # both stderr and stdout to file 
 if [ $? -ne 0 ] ; then # 0 is the exit 
                        # status for a successful command
     exit -1
 fi 
 
-mtca4u_command_2="$mtca4u_executable device_info"
-$mtca4u_command_2 >> $actual_console_output  2>&1 
+
+# print out device info for a card with modules
+echo "Device Info for card with modules" >> $actual_console_output  2>&1
+$mtca4u_executable device_info DUMMY2 >> $actual_console_output  2>&1
+
+
+# Bad command structure
+echo "Device Info command called with incorrect number of parameters" >> $actual_console_output  2>&1
+$mtca4u_executable device_info >> $actual_console_output  2>&1 
 if [ $? -ne 1 ] ; then 
     exit -1
 fi 
